40a09d56ea36963b7e6f0ae760c2d0852a08d50c
Sebastian Graef <segraef@microsoft.com>
2025-06-30 21:33:29 +1000

    refactor: Remove unused data sources and provider configuration from Terraform files

--- data.tf
+++ data.tf
@@ -5,31 +5,3 @@ data "azurerm_resource_group" "existing" {
 
   name = var.resource_group_name
 }
-
-data "azurerm_storage_account" "existing" {
-  count = var.existing_storage_account_resource_id != null && var.existing_storage_account_resource_id != "skip-deployment" ? 1 : 0
-
-  name                = split("/", var.existing_storage_account_resource_id)[8]
-  resource_group_name = split("/", var.existing_storage_account_resource_id)[4]
-}
-
-data "azurerm_key_vault" "existing" {
-  count = var.existing_key_vault_resource_id != null && var.existing_key_vault_resource_id != "skip-deployment" ? 1 : 0
-
-  name                = split("/", var.existing_key_vault_resource_id)[8]
-  resource_group_name = split("/", var.existing_key_vault_resource_id)[4]
-}
-
-data "azurerm_cosmosdb_account" "existing" {
-  count = var.existing_cosmos_db_resource_id != null && var.existing_cosmos_db_resource_id != "skip-deployment" ? 1 : 0
-
-  name                = split("/", var.existing_cosmos_db_resource_id)[8]
-  resource_group_name = split("/", var.existing_cosmos_db_resource_id)[4]
-}
-
-data "azurerm_search_service" "existing" {
-  count = var.existing_ai_search_resource_id != null && var.existing_ai_search_resource_id != "skip-deployment" ? 1 : 0
-
-  name                = split("/", var.existing_ai_search_resource_id)[8]
-  resource_group_name = split("/", var.existing_ai_search_resource_id)[4]
-}
