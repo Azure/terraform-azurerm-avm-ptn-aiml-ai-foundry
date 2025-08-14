31f481cfc6dc4b8507f6837a93fbf5a385d7e919
Sebastian Graef <segraef@microsoft.com>
2025-08-14 20:40:42 +1000

    fix: Correct key vault configuration for BYOR to use module references

--- data.byor.tf
+++ data.byor.tf
@@ -1,6 +1,6 @@
 data "azurerm_key_vault_key" "byor" {
   count = var.create_byor_cmk ? 1 : 0
 
-  key_vault_id = var.ai_foundry.customer_managed_key.key_vault_resource_id
-  name         = var.ai_foundry.customer_managed_key.key_name
+  key_vault_id = module.key_vault.resource_id
+  name         = module.key_vault.keys.cmk.name
 }
