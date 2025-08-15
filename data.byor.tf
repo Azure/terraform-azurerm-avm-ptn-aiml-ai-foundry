52e2da68752e5c019842b2bfb2ac0c7896e0c743
Sebastian Graef <segraef@microsoft.com>
2025-08-15 19:52:05 +1000

    fix: Update customer managed key references in README and adjust key name formatting in BYOR data source

--- data.byor.tf
+++ data.byor.tf
@@ -2,5 +2,5 @@ data "azurerm_key_vault_key" "byor" {
   count = var.create_byor_cmk ? 1 : 0
 
   key_vault_id = try(module.key_vault.resource_id, values({ for k, v in module.key_vault : k => v.resource_id })[0])
-  name = "cmk"
+  name         = "cmk"
 }
