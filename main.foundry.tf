2645a0c2fd33affcb12b3578f47eeed095375775
Sebastian Graef <segraef@microsoft.com>
2025-12-12 10:12:13 +1000

    fix: update API version for Cognitive Services resources to 2025-10-01-preview

--- main.foundry.tf
+++ main.foundry.tf
@@ -2,7 +2,7 @@ resource "azapi_resource" "ai_foundry" {
   location  = var.location
   name      = local.ai_foundry_name
   parent_id = var.resource_group_resource_id
-  type      = "Microsoft.CognitiveServices/accounts@2025-04-01-preview"
+  type      = "Microsoft.CognitiveServices/accounts@2025-10-01-preview"
   body = {
 
     kind = "AIServices",
@@ -42,7 +42,7 @@ resource "azapi_resource" "ai_agent_capability_host" {
 
   name      = "ai-agent-service-${random_string.resource_token.result}"
   parent_id = azapi_resource.ai_foundry.id
-  type      = "Microsoft.CognitiveServices/accounts/capabilityHosts@2025-04-01-preview"
+  type      = "Microsoft.CognitiveServices/accounts/capabilityHosts@2025-10-01-preview"
   body = {
     properties = {
       capabilityHostKind = "Agents"
@@ -62,7 +62,7 @@ resource "azapi_resource" "ai_model_deployment" {
 
   name      = each.value.name
   parent_id = azapi_resource.ai_foundry.id
-  type      = "Microsoft.CognitiveServices/accounts/deployments@2025-04-01-preview"
+  type      = "Microsoft.CognitiveServices/accounts/deployments@2025-10-01-preview"
   body = {
     properties = {
       model = {
