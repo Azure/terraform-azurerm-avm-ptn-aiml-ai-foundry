5c85b298d45b746b5732f464cf7d480b85f834b3
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2026-03-31 18:16:58 -0500

    chore: pre-commit fixes

--- locals.foundry.tf
+++ locals.foundry.tf
@@ -14,7 +14,6 @@ locals {
   cmk_uami = var.ai_foundry.customer_managed_key != null ? {
     (var.ai_foundry.customer_managed_key.user_assigned_identity_resource_id) = {}
   } : null
-
   system_assigned_user_assigned = (var.ai_foundry.managed_identities.system_assigned || length(var.ai_foundry.managed_identities.user_assigned_resource_ids) > 0) || locals.cmk_uami != null ? {
     this = {
       type                       = var.ai_foundry.managed_identities.system_assigned && (length(var.ai_foundry.managed_identities.user_assigned_resource_ids) > 0 || locals.cmk_uami != null) ? "SystemAssigned, UserAssigned" : length(var.ai_foundry.managed_identities.user_assigned_resource_ids) > 0 || locals.cmk_uami != null ? "UserAssigned" : "SystemAssigned"
